114eeccbbf8ef867a4b595721935a0ddc94038e4
Alexis Sellier <alexis@cloudhead.io>
2011-03-16 16:20:01 -0400

    ssh: many improvements

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -5,6 +5,7 @@ source $BIRTH_ROOT/lib/include.sh
 main () {
   local username=$1
   local authorized_keys="/home/$username/.ssh/authorized_keys"
+  local sshd_config="etc/ssh/sshd_config"
 
   log "generating ssh key pair for $username.."
 
@@ -13,7 +14,25 @@ main () {
   log "copying public key to $authorized_keys"
 
   # Copy public key to user's authorized_keys
-  cat /tmp/id_rsa.pub >> $authorized_keys
+  cat $BIRTH_ROOT/id_rsa.pub >> $authorized_keys
+
+  log "copying $BIRTH_ROOT/$sshd_config -> /$sshd_config"
+
+  cp $BIRTH_ROOT/$sshd_config /$sshd_config
+
+  while :; do
+    ask "edit /$sshd_config? [Y/n]" && $EDITOR /$sshd_config
+
+    log "restarting sshd.."
+
+    /etc/rc.d/sshd restart
+
+    ask "now are you able to connect with user $username? [Y/n]" && break
+  done
+
+  ask "restrict ssh access to $username? [Y/n]" && echo "AllowUsers $username" >> /$sshd_config
+
+  return 0
 }
 
 main "$@"
